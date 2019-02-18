f9148ba7f833d2ef66c1ca90a9e96b3a47bf96b1
ChandlerBaskins <43590431+piminto@users.noreply.github.com>
2019-02-17 22:34:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      input: "",
+      tasks: []
+    };
+  }
+
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return(
+        <div>
+          <h1>Tasks</h1>
+          <ul>
+            {this.state.tasks.map((tasks,i) => 
+              <li key ={i}>
+                {tasks}
+                <button data-index={i} className="delete" onClick={this.deleteButton}>
+                  Done
+                </button>
+              </li>
+            )}
+          </ul>
+            <div>
+              <input onChange={this.handleChange} value = {this.state.input}>
+            </input>
+                <button onClick={this.addTask}>Add Tasks</button>
+            </div>
+            <div>
+              <h2>Number of tasks:{this.state.tasks.length}</h2>
+            </div>
+
+        </div>
+
+
     );
   }
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+//add task funct
+  addTask = () => {
+    const input = this.state.input;
+    if(input.length > 0) {
+
+    
+    this.setState(state => ({
+      tasks: [...state.tasks, state.input],
+      input: ""
+    }))
+      }
+  }
+
+// delete button funct
+deleteButton = (event) => {
+  const index = event.target.dataset.index
+  this.setState(state => {
+    const tasks = [...state.tasks];
+    tasks.splice(index, 1); 
+    return {
+      tasks: tasks
+    }
+      
+    
+  })
 }
+
+}
+
 
 export default App;
